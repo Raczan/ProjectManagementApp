6c7cd1f9068155f7bed9606e5f1df74c5071d9a6
Raczan <51896000+Raczan@users.noreply.github.com>
2025-05-16 10:11:31 -0600

    feat: Add basic sidebar structure

--- ProjectManagementApp/MainWindow.xaml.cpp
+++ ProjectManagementApp/MainWindow.xaml.cpp
@@ -6,12 +6,19 @@
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Controls;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace winrt::ProjectManagementApp::implementation
 {
+    void MainWindow::InitializeComponent()
+    {
+        MainWindowT::InitializeComponent();
+        LoadProjects();
+    }
+
     int32_t MainWindow::MyProperty()
     {
         throw hresult_not_implemented();
@@ -20,5 +27,31 @@ namespace winrt::ProjectManagementApp::implementation
     void MainWindow::MyProperty(int32_t /* value */)
     {
         throw hresult_not_implemented();
+    }
+
+    void MainWindow::LoadProjects()
+    {
+        try {
+            auto projectsNavItem = ProjectsNavItem();
+
+            if (projectsNavItem == nullptr) {
+                return;
+            }
+
+            projectsNavItem.MenuItems().Clear();
+
+            for (const auto& projectName : projectsList)
+            {
+                NavigationViewItem item;
+                item.Content(box_value(projectName));
+                projectsNavItem.MenuItems().Append(item);
+            }
+        }
+        catch (winrt::hresult_error const& ex) {
+            OutputDebugString((L"Error en LoadProjects: " + std::wstring(ex.message()) + L"\n").c_str());
+        }
+        catch (...) {
+            OutputDebugString(L"Error desconocido en LoadProjects\n");
+        }
     }
 }
--- ProjectManagementApp/MainWindow.xaml.h
+++ ProjectManagementApp/MainWindow.xaml.h
@@ -12,8 +12,18 @@ namespace winrt::ProjectManagementApp::implementation
             // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
         }
 
+        void InitializeComponent();
         int32_t MyProperty();
         void MyProperty(int32_t value);
+        void LoadProjects();
+
+    private:
+        std::vector<std::wstring> projectsList = {
+            L"Proyecto Interactivo",
+            L"Gestión de Hotel",
+            L"Sistema de Contabilidad",
+            L"Administración de Miembros"
+        };
     };
 }
 
